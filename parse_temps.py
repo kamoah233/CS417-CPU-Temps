3e21622447ddb16d44775baf5f384817f5a5f679
Kofi <112496906+kamoah233@users.noreply.github.com>
2023-10-12 19:13:28 -0400

    Create parse_temps.py

---
+++ parse_temps.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+"""
+Taken from Professor Kennedy's Github repository
+Link- https://github.com/cstkennedy/cs417-examples/tree/master/SemesterProject-CPU-Temps
+
+This module is a collection of input helpers for the CPU Temperatures Project.
+All code may be used freely in the semester project, if it is imported using 
+''import parse_temps'' or ''from parse_temps import {...}'' where ''{...}''
+represents one or more functions.
+"""
+
+import re
+from typing import (TextIO, List, Tuple)
