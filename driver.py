ece45978b642928c527e59597ba65c27621d3dfa
Kofi <kofilarbiamoah@gmail.com>
2023-10-23 12:53:46 -0400

    added driver

---
+++ driver.py
@@ -0,0 +1,18 @@
+import sys 
+
+from parse_temps import parse_raw_temps
+
+def main (args: List[str]) -> int:
+    """
+    The main function for the CPU Temperatures Project.
+    """
+    ###################################################################
+    # Parse the input file
+    with open (args[1], "r") as original_temps:
+        temps = list(parse_raw_temps(original_temps))
+    
+    # Print the parsed data
+    for time, core_temps in temps:
+        print (time, core_temps)
+    
+    return 0
